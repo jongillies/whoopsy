0d5df56601f97d19d12463b2f9bd6b8623db8394
Jon Gillies <jon@gillies.us>
2013-07-29 18:26:48 -0700

    Updated with CLI arg work

--- lib/whoopsy.rb
+++ lib/whoopsy.rb
@@ -5,21 +5,6 @@ require 'whoopsy/config'
 
 module Whoopsy
   class Logger
-    attr_reader :cli_arg
-    def initialize(args={}, version_text='', banner_text='')
-      opts = Trollop::options(args) do
-        banner = banner_text
-        version = version_text
-        opt :verbose, 'Verbose output.', short: 'v'
-        opt :debug, 'Display additional debugging information.'
-      end
-      unless @cli_arg = args[0]
-        Trollop::die 'ERROR: You must specify something on the command line.'
-      end
-      opts
 
-      @config = Whoopsy::Config.new()
-
-    end
   end
 end
--- lib/whoopsy/config.rb
+++ lib/whoopsy/config.rb
@@ -1,10 +1,9 @@
 require 'yaml'
 require 'logger'
-require 'active_support/core_ext/hash'
 
 
 module Whoopsy
-  config_options = [:host,
+  CONFIG_OPTIONS = [:host,
                     :http_open_timeout,
                     :http_read_timeout,
                     :port,
@@ -20,13 +19,11 @@ module Whoopsy
     attr_accessor :config
 
 
-    def initialize(args={})
-
-      @logger = args.fetch(:logger) { Logger.new(STDOUT) }
+    def initialize
 
       @config = {}
 
-      @file_name = 'xxwhoopsy.yml'
+      @file_name = 'whoopsy.yml'
       @global_config_file = "/etc/#{@file_name}"
       @local_config_file = "#{File.join(ENV['HOME'],".#{@file_name}")}"
       @environment_file = ENV['WHOOPSY_CONFIG']
@@ -36,19 +33,15 @@ module Whoopsy
       load_file @local_config_file
       load_file @environment_file if @environment_file
 
-      puts "asdf"
     end
 
     private
 
     def load_file(file_name)
       begin
-        @config.reverse_merge! YAML::load_file(file_name)
-      rescue Errno::ENOENT => e
-        puts "#{file_name} does not exist"
-      rescue TypeError
-        puts "#{file_name} fuck"
-
+        @config.merge! YAML::load_file(file_name)
+      rescue Errno::ENOENT
+        $stderr.puts "WARNING: #{file_name} does not exist"
       end
 
     end
