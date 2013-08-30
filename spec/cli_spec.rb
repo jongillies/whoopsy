d1d9ea5dc4cd02cce19a1a1b319841cef4b380d5
Jon Gillies <jon@gillies.us>
2013-08-30 13:26:20 -0700

    Updated CLI spec to do integration testing against a local copy

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 require 'whoopsy'
 require 'shellwords'
 require 'yaml'
+require 'logger'
 
 describe 'Different Command Line Options' do
 
-  it 'should fail with no options' do
-    expect { Whoopsy::Log.new({}) }.to raise_error
-  end
+  #it 'should fail with no options' do
+  #  expect { Whoopsy::Log.new({}) }.to raise_error
+  #end
 
   it 'should succeed with required arguments' do
-    args = "--event-type 'warning' --service 'my script' --environment 'development' --message 'working ok today' --event-group-identifier 'whats this'"
+    args = "--event-type 'warning' --service 'my script' --environment 'development' --event-group-identifier 'whats this'"
     expect { Whoopsy::Log.new(args.shellsplit) }.not_to raise_error
   end
 
   it 'should fail missing --environment' do
-    args = "--event-type 'warning' --service 'my script' --message 'working ok today' --event-group-identifier 'whats this'"
+    args = "--event-type 'warning' --service 'my script' --event-group-identifier 'whats this'"
     expect { Whoopsy::Log.new(args.shellsplit) }.to raise_error
   end
 
@@ -33,37 +34,40 @@ end
 
 #describe 'Handling --details' do
 #
+#  HOST = 'localhost'
+#  PORT = 9292
+#
 #  it 'should parse --details from the command line' do
 #
 #    details = { first_name: 'Scooby', last_name: 'Doo', age: 7, friends: %w(Fred Daphne Velma Shaggy) }
 #
-#    args = "--event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details \"#{details.to_json}\""
+#    args = "--host #{HOST} --port #{PORT} --debug --event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details '#{details.to_json}'"
 #
 #    expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
 #  end
 #
-#  #it 'should parse --details from a file' do
-#  #
-#  #  file = 'spec/data/good.json'
-#  #  args = "--event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
-#  #
-#  #  expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
-#  #end
-#  #
-#  #it 'should fail if --details cannot be cracked' do
-#  #
-#  #  file = 'spec/data/bad.json'
-#  #  args = "--event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
-#  #
-#  #  expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
-#  #end
-#  #
-#  #it 'should fail if --details file is empty' do
-#  #
-#  #  file = 'spec/data/empty.json'
-#  #  args = "--event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
-#  #
-#  #  expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
-#  #end
+#  it 'should parse --details from a file' do
+#
+#    file = 'spec/data/good.json'
+#    args = "--host #{HOST} --port #{PORT} --debug --event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
+#
+#    expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
+#  end
+#
+#  it 'should fail if --details cannot be cracked' do
+#
+#    file = 'spec/data/bad.json'
+#    args = "--host #{HOST} --port #{PORT} --debug --event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
+#
+#    expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
+#  end
+#
+#  it 'should fail if --details file is empty' do
+#
+#    file = 'spec/data/empty.json'
+#    args = "--host #{HOST} --port #{PORT} --debug --event-type \"warning\" --service \"my script\" --environment \"development\" --message \"working ok today\" --event-group-identifier \"whats this\" --details @#{file}"
+#
+#    expect { Whoopsy::Log.new(args.shellsplit)}.not_to raise_error
+#  end
 #
 #end
